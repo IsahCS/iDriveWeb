1270ef9747e99021e06754de3407267a66c94008
isabel <isabel.silva@proradis.com.br>
2024-11-27 09:11:48 -0300

    Refactor Map component to improve image rendering and responsiveness

--- src/app/Map.tsx
+++ src/app/Map.tsx
@@ -11,13 +11,27 @@ const Map: React.FC<MapProps> = ({ mapUrl }) => {
     <Box
       sx={{ 
         borderRadius: 6,
-        overflow: 'hidden',
-        boxShadow: '0px 0px 10px 0px #00000033',
-        width: '100%',
-        height: "100%",
+        overflow: "hidden",
+        boxShadow: "0px 0px 10px 0px #00000033",
+        width: "100%",
+        height: { xs: "50%", md: "100%" },
+        position: "relative",
       }}
     >
-      <img src={mapUrl} alt="Mapa da rota" width="100%" height="100%" />
+      <Box
+        component="img"
+        src={mapUrl}
+        alt="Mapa da rota"
+        sx={{
+          width: "100%",
+          height: "100%",
+          objectFit: "cover",
+          position: "absolute",
+          top: 0,
+          left: 0,
+        }}
+      />
+      <img src={mapUrl} alt="Mapa da rota" width="500%" height="300%" />
     </Box>
   )
 }
